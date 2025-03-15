2f87889a7a1a5a6e5df74617d4d6622111f6b9a4
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2025-03-15 23:46:03 +0530

    Update sketch.js

--- 00_boilerplate/sketch.js
+++ 00_boilerplate/sketch.js
@@ -2,11 +2,13 @@
 var canvasWidth = 700;
 var canvasHeight = 700;
 
+var backgroundGrey = 51;
+
 function setup() {
   createCanvas(canvasWidth, canvasHeight);
 }
 
 function draw() {
-  background(51);
+  background(backgroundGrey);
   stroke(255);
 }
